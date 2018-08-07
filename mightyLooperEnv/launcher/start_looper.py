025cc546d9770085885313d58d8d9485fab1f0ce
pstruho <sefcik.anton@gmail.com>
2018-08-07 23:51:59 +0200

    Added scripts for starting bluetooth service and launching mighty_looper.pd

---
+++ mightyLooperEnv/launcher/start_looper.py
@@ -0,0 +1,16 @@
+import bt_service
+import signal
+
+def signal_handler(sig, frame):
+        print('SIGINT received')
+        bt_service.clean_up()
+        sys.exit(0)
+
+# register handler for interrupt signal
+signal.signal(signal.SIGINT, signal_handler)
+
+# start bluetooth service
+bt_service.start()
+
+# run Pure Data patch
+os.system('sudo pd -nogui -path /home/pi/mighty_looper/puredata/pd_externals/ -lib mighty_looper_lib -audiobuf 5 /home/pi/mighty_looper/puredata/mighty_looper.pd')
