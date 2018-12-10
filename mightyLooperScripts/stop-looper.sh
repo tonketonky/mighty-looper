972ad8300ce5d0b7c0f2c3828c5298985ae21664
tonycarrotti <sefcik.anton@gmail.com>
2018-12-10 01:15:33 +0100

    Corrected indentation

--- mightyLooperScripts/stop-looper.sh
+++ mightyLooperScripts/stop-looper.sh
@@ -8,11 +8,11 @@ if [ ! -f "$SCRIPTS_DIR/.ml.pid" ]
         echo "Process of mighty_looper.py has to be killed manually."
     else
         kill $(cat "$SCRIPTS_DIR/.ml.pid")
-		if [ $? -eq 0 ]
+        if [ $? -eq 0 ]
             then
                 rm "$SCRIPTS_DIR/.ml.pid"
                 echo "Mighty Looper stopped"
             else
                 echo "Unable to stop Mighty Looper"
-    	fi
+        fi
 fi
